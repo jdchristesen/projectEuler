d5080fe98ffe33f97673c7149c53bbc5a53603ef
Joseph Christesen <joseph.christesen@mpq.mpg.de>
2017-05-19 16:58:49 +0200

    Finished summations of primes

---
+++ summations_of_primes.py
@@ -0,0 +1,22 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 ==0:
+        return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
+
+summation = 0
+for i in range(2000000):
+    if is_prime(i):
+        summation += i
+
+print(summation)
