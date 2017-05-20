4c44cdc8bd537315d3740a964ce5411cefe5275e
Joseph Christesen <jdchristesen@gmail.com>
2017-05-20 09:34:14 +0200

    Finished highly divisible triangular number and large sum

--- highly_divisible_triangular_number.py
+++ highly_divisible_triangular_number.py
@@ -1,15 +1,23 @@
 def num_of_divisors(n):
-    i = 1
-    divisor_count = 0
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        divisor_count = 2
+    i = 2
     while i * i <= n:
         if n % i == 0:
-            divisor_count += 1
+            divisor_count += 2
         i += 1
     return divisor_count
 
 count = 0
 natural_numbers = [1]
-while count <= 500:
+while count < 500:
+    # print('Sum ', sum(natural_numbers))
     count = num_of_divisors(sum(natural_numbers))
-    print(count)
+    # print('Count ', count)
     natural_numbers.append(natural_numbers[-1]+1)
+
+print(sum(natural_numbers[:-1]))
