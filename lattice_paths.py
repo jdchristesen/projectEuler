88437c6e0bd5445ea57a529ecdf4e5a0d8611e38
Joseph Christesen <joseph.christesen@mpq.mpg.de>
2017-05-22 14:29:40 +0200

    Finished lattice paths

--- lattice_paths.py
+++ lattice_paths.py
@@ -1,11 +1,11 @@
+import math
+
 def pascal_row_sum(n):
     if n == 0:
         return 1
-    elif n == 1:
-        return 2
+
     prev_row = [1, 1]
-    total_sum = sum(prev_row)
-    for i in range(2,n+1):
+    for i in range(1, n):
         next_row = [0 for x in range(len(prev_row)+1)]
         next_row[0] = 1
         next_row[len(prev_row)] = 1
@@ -13,8 +13,18 @@ def pascal_row_sum(n):
             next_row[j+1] = prev_row[j] + prev_row[j+1]
         print(next_row)
         prev_row = next_row
-        total_sum += sum(next_row)
 
-    return total_sum
+    for i in range(n, 0, -1):
+        next_row = [0 for x in range(len(prev_row)-1)]
+        for j in range(1, len(prev_row)):
+            next_row[j-1] = prev_row[j-1] + prev_row[j]
+        print(next_row)
+        prev_row = next_row
 
-print(pascal_row_sum(21))
+    return next_row
+
+cube_size = 20
+print(pascal_row_sum(cube_size))
+
+# This is 2*cube_size choose cube_size
+print(math.factorial(2*cube_size) / (math.factorial(cube_size) ** 2))
