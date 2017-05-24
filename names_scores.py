8f89dbf2410ac2864e2abc93e5681513fa1edaf6
Joseph Christesen <joseph.christesen@mpq.mpg.de>
2017-05-24 11:47:36 +0200

    Finished names scores

--- names_scores.py
+++ names_scores.py
@@ -6,14 +6,14 @@ with open('p022_names.txt') as fp:
         names += line
 
 names = names.split(',')
+names = [x.strip('"') for x in names]
 names = sorted(names)
 
 total = 0
 for i, name in enumerate(names):
-    name = name.strip('"')
     temp = 0
     for letter in name:
         temp += alpha.index(letter) + 1
-    total += i * temp
+    total += (i + 1) * temp
 
 print(total)
