2007ca0b0267882eef3074785d46b7aef67ade03
Joseph Christesen <joseph.christesen@mpq.mpg.de>
2017-06-01 09:26:32 +0200

    Finished circular primes

---
+++ circular_primes.py
@@ -0,0 +1,38 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
+
+circular_primes = []
+not_circular = set()
+for x in range(500000):
+    # if ''.join(sorted(str(x))) in not_circular:
+    #     continue
+    if x not in circular_primes and is_prime(x):
+        circular_bool = True
+        circular = x
+        for i in range(len(str(circular))):
+            circular = int(str(circular)[-1] + str(circular)[:-1])
+            if not is_prime(circular):
+                circular_bool = False
+
+        if circular_bool:
+            circular = x
+            for i in range(len(str(circular))):
+                circular = int(str(circular)[-1] + str(circular)[:-1])
+                circular_primes.append(circular)
+        else:
+            not_circular.add(''.join(sorted(str(x))))
+
+print(len(sorted(set(circular_primes))))
